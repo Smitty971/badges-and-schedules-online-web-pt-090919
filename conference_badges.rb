ea6e1d7367b56ccb18c5c697dc6ccbdebfad0a37
Jamal Turner <jlsmitty2273@yahoo.com>
2019-09-19 12:17:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,6 +15,7 @@ def assign_rooms(attendees)
   new_array = []
   attendees.each do |name|
       new_array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+      counter += 1
     end
       new_array
   end
