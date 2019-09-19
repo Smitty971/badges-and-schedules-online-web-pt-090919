9c1bd5605ba5dbcd231edb204301cee94e9b2a68
Jamal Turner <jlsmitty2273@yahoo.com>
2019-09-19 11:44:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def badge_maker(name)
+   "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  new_array = []
+  attendees.each do |name| 
+  new_array.push("Hello, my name is #{name}.")
+  end
+  return new_array
+end
+
+def assign_rooms(attendees)
+  
+  end
